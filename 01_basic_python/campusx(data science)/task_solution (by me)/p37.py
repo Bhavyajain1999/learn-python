b369bee32d29d1f90b109f7f2031bdec6d5361cf
One <BHAVYAJAIN064@GMAIL.COM>
2024-02-13 09:46:55 +0530

    learn

--- 01_basic_python/campusx(data science)/task_solution (by me)/p37.py
+++ 01_basic_python/campusx(data science)/task_solution (by me)/p37.py
@@ -1,8 +1,14 @@
 # Combine two lists index-wise(columns wise)
-list1 = ['m','na','i','bha']
-list2 = ['y','me','s','vya']
+list1 = ["m","na","i","bha"]
+list2 = ["y","me","s","vya"]
 #mehtod1
 output = list(zip(list1,list2))
 print(output)
 
 #method2
+
+# # Write code here
+# list1 = ["M", "na", "i", "Kh"]
+# list2 = ["y", "me", "s", "an"]
+
+# [[i,j] for (i,j) in zip(list1,list2)]
